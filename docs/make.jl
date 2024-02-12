03a1c713963515811ca71cf41cdd250a4c79f0a1
JoelTrent <79883375+JoelTrent@users.noreply.github.com>
2024-02-12 17:37:22 +1300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -21,7 +21,7 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
-        "Workflow Background" => "workflow_background.md",
+        "Background" => "workflow_background.md",
         "User Interface" => 
             ["Initialisation" => "user_interface/initialisation.md",
             "Parameter Profiles and Samples" => 
